323e9b61709393c3132c84668204722aad441e8c
Atla004 <116417387+Atla004@users.noreply.github.com>
2023-02-04 17:31:56 -0400

    Add files via upload

---
+++ Listas.c
@@ -0,0 +1,88 @@
+  #include<stdio.h>
+#include<stdlib.h>
+
+
+typedef struct nodo{
+
+    int dato;
+    struct nodo * siguiente;
+
+}NODO;
+
+ NODO * CrearNodo(int dato);
+ int InsertarInicio(NODO **cabeza, int dato);
+ int InsertarFinal(NODO **cabeza, int dato);
+ void ImprimirLista(NODO *cabeza);
+
+int main (){
+
+    NODO *cabeza = NULL ;
+    int res ;
+
+    InsertarInicio(&cabeza, 3);
+    InsertarFinal(&cabeza, 0);
+    InsertarFinal(&cabeza, 4);
+    InsertarFinal(&cabeza, 6);
+    InsertarFinal(&cabeza, 5);
+    InsertarFinal(&cabeza, 1);
+    InsertarFinal(&cabeza, 7);
+    InsertarFinal(&cabeza, 4); 
+    ImprimirLista(cabeza);
+
+
+    return 0 ;
+}
+
+NODO * CrearNodo(int dato){
+
+    NODO *nuevo = NULL;
+
+    nuevo = (NODO*)malloc (sizeof(NODO));
+
+    if (nuevo != NULL){
+        nuevo ->dato = dato;
+        nuevo ->siguiente = NULL;
+    }
+
+    return nuevo;
+}
+ int InsertarInicio(NODO **cabeza, int dato){
+
+     NODO  *nuevo = NULL;
+     nuevo = CrearNodo(dato);
+     if(nuevo != NULL){
+        nuevo ->siguiente = *cabeza;
+        *cabeza =nuevo;
+        return 1;
+     }
+
+     return 0 ;
+ }
+
+void ImprimirLista(NODO *cabeza){
+
+    NODO *nAux = cabeza;
+
+    while( nAux != NULL){
+        printf("%d ", nAux ->dato);
+        nAux= nAux ->siguiente;
+    }
+
+  }
+int InsertarFinal(NODO **cabeza, int dato){
+
+   NODO  *nuevo = NULL, *nAux= * cabeza;
+
+   nuevo = CrearNodo(dato);
+
+   if( nuevo != NULL){
+        while(nAux ->siguiente!=NULL){
+            nAux  = nAux ->siguiente;
+        }
+        nAux ->siguiente= nuevo;
+        return 1;
+   }
+
+   
+ }
+ 
